7882fd62b39ddded8eaf662cf00b6759c9554237
Bartosz Janowski <detnotatka@gmail.com>
2023-03-23 00:47:00 +0100

    Tiny change in Main for ready simple showcase

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,24 @@
 import mas.model.Employee;
 
+import java.io.FileNotFoundException;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        Employee emp = new Employee(1, "Mark", "Town", "Java", "Warsaw", "Katiuszy",
-                "Poland", "04-453", "PKO", "09854987539446");
+//        Employee emp = new Employee(1, "Mark", "Town", "Java", "Warsaw", "Katiuszy",
+//                "Poland", "04-453", "PKO", "09854987539446");
+//
+//        Employee emp2 = new Employee(2, "Pant", "John", "Python", 1, "Warsaw", "Rybacka",
+//                "Poland", "04-547", "PKO", "56347856835654");
 
-        Employee emp2 = new Employee(2, "Pant", "John", "Python", 1, "Warsaw", "Rybacka",
-                "Poland", "04-547", "PKO", "56347856835654");
+//        Employee.saveExtent("test.dat");
 
-        List<Employee> test = Employee.getExtent();
-        System.out.println(test.get(0));
-        System.out.println(emp2);
+        try {
+            Employee.loadExtent("test.dat");
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+
+        System.out.println(Employee.getExtent());
     }
 }
